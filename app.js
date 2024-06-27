7d6ec843d90d2bc8a965f50cf6275d13891650ef
Nay Ba La <naybala.zotefamily@gmail.com>
2024-06-27 17:09:49 +0630

    add styling

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-var width = 320; // We will scale the photo width to this
-var height = 0; // This will be computed based on the input stream
-
+var width = 320;
+var height = 0;
 var streaming = false;
 
 var video = null;
